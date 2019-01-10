38c790342ee78d7eced13634d237b667183033ae
dev-pasa <dev5@uw.edu>
2019-01-10 11:50:59 -0800

    liked not test

---
+++ Data-Structures/LinkedList/LinkedList/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+      public class Node
+    {
+        public int Value;
+        public Node Next;
+        
+
+        public Node(int value)
+        {
+         Value = value;
+        
+
+        }
+
+    }
+}
+
