02cfec11501243d47935c3cd51ac5b190a8e677c
dev-pasa <dev5@uw.edu>
2019-01-10 10:58:52 -0800

    fix

---
+++ Data-Structures/LinkedList/LinkedList/MyLinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class MyLinkedList
+    {
+
+        //default empty value in head
+        //insert value to the head
+        // whether the value exists
+        // print the all the values
+
+        public class Node
+        {
+            public int Value;
+            public Node Next;
+            public Node(int value)
+            {
+                this.Value = value;
+            }
+            
+        }
+        public class LinkedList
+        {
+            public Node head;
+
+            public void PrintAllheads()
+            {
+                Node current = head;
+                while(current != null)
+                {
+                    Console.WriteLine(current.Value);
+                    current = current.Next;
+                }
+            }
+
+            public void AddToHead(int value)
+            {
+                Node newNode = new Node(value);
+                //question here
+                newNode.Value = value;
+                newNode.Next = head;
+                head = newNode;
+            }
+
+            public static bool CheckValue(int Value)
+                Node current = head;
+
+            }
+        }
+
+
+    }
+}
