cf8d2b176878a6d317e2027047c1c3e95432ede7
dev-pasa <dev5@uw.edu>
2019-01-16 09:51:26 -0800

    Kth node

---
+++ Challenges/KthNodeFromEnd/KthNodeFromEnd/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using LinkedList;
+
+namespace KthNodeFromEnd
+{
+    public class Program
+    {
+        /// <summary>
+        /// The main function to run the code
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Node node1 = new Node(1);
+            MyLinkedList list = new MyLinkedList(node1);
+            list.Append(2);
+            list.Append(3);
+            list.Append(4);
+            list.Append(5);
+
+            list.Print();
+            Console.WriteLine("1st node from end");
+            Console.WriteLine(KthNodeEnd(list, 1));
+
+            Console.WriteLine("2nd node from end");
+            Console.WriteLine(KthNodeEnd(list, 2));
+        }
+
+        /// <summary>
+        /// Find the Kth node from the end
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="k"></param>
+        /// <returns>int value of Node</returns>
+        public static int KthNodeEnd(MyLinkedList list, int k)
+        {
+            if (list.Head == null)
+            {
+                throw new NullReferenceException("List is null");
+            }
+            
+            int count = 0;
+            Node current = list.Head;
+
+            while (current.Next != null)
+            {
+                count++;
+                current = current.Next;
+            }
+
+            if (count < k)
+            {
+                throw new IndexOutOfRangeException("Index out of range.");
+            }
+
+            current = list.Head;
+
+            while (count > k)
+            {
+                current = current.Next;
+                count--;
+            }
+            return current.Value;
+        }
+    }
+}
---
+++ Challenges/KthNodeFromEnd/README.md
@@ -0,0 +1,14 @@
+#Find the Kth node from the last node
+
+#Challenge
+Write a function where given a list, find the kth node from the last node.
+
+
+#Solution
+I did two loops for this problem. First to find the count of the list, then to count backward to the kth node. Given a kth value, when the loop starts from the end, the current head stops at the kth node. 
+
+#BigO for this problem is O(n).
+
+
+#WhiteBoard
+![whiteboard](/Assets/whiteboard.JPG)
