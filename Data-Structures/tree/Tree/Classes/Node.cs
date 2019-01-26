1d0cb1a88cd7a7f63c3b180bb15b38ae53538e16
dev-pasa <dev5@uw.edu>
2019-01-26 15:43:22 -0800

    code

---
+++ Data-Structures/tree/Tree/Classes/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    public class Node
+    {
+        /// <summary>
+        /// value property
+        /// </summary>
+        public int Value { get; set; }
+
+        //node left and right
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        /// <summary>
+        /// Initialize the value of the root
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(int value)
+        {
+            Value = value;
+            Left = Right = null;
+        }
+    }
+}
