8f12a1e8e72afe8872a4293b259977bc8a94617e
dev-pasa <dev5@uw.edu>
2019-01-28 20:39:08 -0800

    Fiz Buzz

---
+++ Challenges/FizzBuzzTree/README.md
@@ -0,0 +1,17 @@
+# FizzBuzz Tree
+
+
+## Challenge
+Conduct “FizzBuzz” on a tree while traversing through it. Change the values of each of the nodes dependent on the current node’s value.
+Instead of changing the values of the nodes, output to the console the new values respectively.
+
+
+## Approach & Efficiency
+Time Complexity = O(n): Must traverse the whole tree
+Space Compexity = O(h): Where h is the height of the tree
+
+## Solution
+![FizzBuzz tree](/Assets/FizzBuzzTree.jpg)
+![FizzBuzz](/Assets/FizzBuzzTree2.jpg)
+
+
---
+++ Challenges/XUnitTestFizzBuzzTree/UnitTest1.cs
@@ -0,0 +1,51 @@
+using System;
+using Xunit;
+using static FizzBuzzTree.Program;
+using Tree.Classes;
+namespace XUnitTestFizzBuzzTree
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void FizzBuzzTest1()
+        {
+            BinaryTree BT = new BinaryTree(5);
+            BT.root.Left = new Node(11);
+            BT.root.Left.Left = new Node(10);
+            BT.root.Left.Right = new Node(15);
+            BT.root.Right = new Node(12);
+            BT.root.Right.Right = new Node(9);
+            BT.root.Right.Left = new Node(17);
+
+            FizzBuzzTree.Program.FizzBuzzTree(BT);
+        }
+
+        [Fact]
+        public void FizBuzzTest2()
+        {
+            BinarySearchTree BST = new BinarySearchTree();
+            BST.root = BST.Add(BST.root, 10);
+            BST.root = BST.Add(BST.root, 15);
+            BST.root = BST.Add(BST.root, 5);
+            BST.root = BST.Add(BST.root, 8);
+            BST.root = BST.Add(BST.root, 12);
+            BST.root = BST.Add(BST.root, 19);
+
+            FizzBuzzTree.Program.FizzBuzzTree(BST);
+        }
+
+        [Fact]
+        public void FizBuzzTest3()
+        {
+            BinarySearchTree BST = new BinarySearchTree();
+            BST.root = BST.Add(BST.root, 12);
+            BST.root = BST.Add(BST.root, 13);
+            BST.root = BST.Add(BST.root, 5);
+            BST.root = BST.Add(BST.root, 18);
+            BST.root = BST.Add(BST.root, 22);
+            BST.root = BST.Add(BST.root, 29);
+
+            FizzBuzzTree.Program.FizzBuzzTree(BST);
+        }
+    }
+}
