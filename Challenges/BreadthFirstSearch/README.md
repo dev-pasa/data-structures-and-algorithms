7f710bcb27d5bcf40d57071061bbc7ae0c2251ae
dev-pasa <dev5@uw.edu>
2019-01-29 21:12:30 -0800

    breadth first

---
+++ Challenges/BreadthFirstSearch/README.md
@@ -0,0 +1,14 @@
+# Breadth First Binary Tree Traversal
+Breadth first traversal of a Tree data structure.
+
+## Challenge
+Write a breadth first traversal method which takes a Binary Tree as its unique input. Without utilizing any of the built-in methods available to your language, traverse the input tree using a Breadth-first approach; print every visited node’s value.
+
+## Approach & Efficiency
+The Big O of the algorithm is O(N). I will traverse through the entire nodes in the binary tree. 
+
+
+## Solution
+
+markup: ![Breadth First](/Assets//BreadthFirst.jpg)
+markup: ![Breadth First2](/Assets//BreadthFirst.jpg)
---
+++ Challenges/XUnitTestBreadthFirst/UnitTest1.cs
@@ -0,0 +1,60 @@
+using System;
+using Xunit;
+using BreadthFirst;
+using Tree.Classes;
+
+namespace XUnitTestBreadthFirst
+{
+    public class UnitTest1
+    {   
+        /// <summary>
+        /// Test if the method runs on a Binary Tree
+        /// </summary>
+        [Fact]
+        public void BreadthTestBT1()
+        {
+            BinaryTree tree = new BinaryTree(1);
+            tree.root.Left = new Node(2);
+            tree.root.Right = new Node(3);
+            tree.root.Left.Left = new Node(4);
+            tree.root.Left.Right = new Node(5);
+            tree.root.Right.Left = new Node(6);
+            
+            Program.BreadthFirstTraversal(tree);
+        }
+
+        /// <summary>
+        /// Test if the method runs on a Binary Search Tree
+        /// </summary>
+        [Fact]
+        public void BreadthBSTTest2()
+        {
+            BinarySearchTree BST = new BinarySearchTree();
+            BST.Add(BST.root, 1);
+            BST.Add(BST.root, 13);
+            BST.Add(BST.root, 15);
+            BST.Add(BST.root, 28);
+            BST.Add(BST.root, 22);
+            BST.Add(BST.root, 29);
+
+            Program.BreadthFirstTraversal(BST);
+        }
+
+        /// <summary>
+        /// Test if the method runs on a Binary Search Tree
+        /// </summary>
+        [Fact]
+        public void BreadthBSTTest3()
+        {
+            BinarySearchTree BST = new BinarySearchTree();
+            BST.Add(BST.root, 22);
+            BST.Add(BST.root, 43);
+            BST.Add(BST.root, 45);
+            BST.Add(BST.root, 18);
+            BST.Add(BST.root, 22);
+            BST.Add(BST.root, 9);
+
+            Program.BreadthFirstTraversal(BST);
+        }
+    }
+}
