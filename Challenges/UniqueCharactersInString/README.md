e7f0d1993b8041b1cba066f53b479390a52f3363
dev-pasa <dev5@uw.edu>
2019-02-24 08:29:23 -0800

    unique chars

---
+++ Challenges/UniqueCharactersInString/README.md
@@ -0,0 +1,23 @@
+# Unique Characters in string
+Create a method to check if the string contains unique charcters using HashSet.
+
+## Challenge
+* Check whether a string has all unique charcters 
+* Return a boolean value to indicate whether the string has unique charcters
+
+
+## Approach 
+* Create a hashset
+* Loop through the entire string and place every string in the hash set
+* If the hashset already contains the character return false else true if all unique characters.
+
+## Efficiency
+* Time Complexity = O(n): Must traverse the entire string
+* Space Compexity = O(l): Where L is length of the string
+
+## Solution
+![String Unique Characters](/Assets/UniqueCharacters.jpg)
+
+
+
+
---
+++ Challenges/UniqueCharactersInString/UniqueCharcter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueCharcter
+{
+    public class Program
+    {
+        //implementing the method
+        public static void Main(string[] args)
+        {
+            string input = "Worldd!";
+            Console.WriteLine(UniqueCharacters(input));
+        }
+
+
+        /// <summary>
+        /// Create a method that checks if a string has unique characters
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool UniqueCharacters(string input)
+        {
+            //create a hasset called set
+            HashSet<char> set = new HashSet<char>(input.Length);
+            //put all the values in the set
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!set.Add(input[i]))
+                  return false;
+            }
+            return true;
+        }
+    }
+}
