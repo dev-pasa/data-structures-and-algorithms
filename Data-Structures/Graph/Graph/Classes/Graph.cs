e5f8b005e4383fd23e9e4a8a151a1050501ea201
dev-pasa <dev5@uw.edu>
2019-02-25 04:53:39 -0800

    graph new

---
+++ Data-Structures/Graph/Graph/Classes/Graph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph.Classes
+{
+    class Graph
+    {
+
+        public List<Vertex> Vertices { get; set; }
+
+
+        public Graph(List<object> values)
+        {
+            Vertices = new List<Vertex>();
+            foreach (object value in values)
+            {
+                Vertices.Add(new Vertex(value));
+            }
+        }
+
+        public int Size()
+        {
+            return Vertices.Count;
+        }
+
+    }
+}
