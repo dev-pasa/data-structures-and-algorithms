f037d65ac56511825993cbfe8de3c21887f711f2
dev-pasa <dev5@uw.edu>
2019-03-10 15:05:34 -0700

    Max Depth

---
+++ Challenges/MaxDepthBinaryTree/BinaryTreeMaxDepth/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tree;
+using Tree.Classes;
+
+namespace BinaryTreeMaxDepth
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree BST = new BinarySearchTree();
+            BST.root = BST.Add(BST.root, 10);
+            BST.root = BST.Add(BST.root, 15);
+            BST.root = BST.Add(BST.root, 5);
+            BST.root = BST.Add(BST.root, 8);
+            BST.root = BST.Add(BST.root, 12);
+            BST.root = BST.Add(BST.root, 19);
+            Console.WriteLine(MaxDepth(BST.root));
+        }
+
+
+        public static int MaxDepth(Node root)
+        {
+            if (root == null) return 0;
+            else
+            {
+                int LeftDepth = MaxDepth(root.Left);
+                int RightDepth = MaxDepth(root.Right);
+
+                return (Math.Max(LeftDepth, RightDepth)+1);
+            }      
+        }
+        
+    }
+}
