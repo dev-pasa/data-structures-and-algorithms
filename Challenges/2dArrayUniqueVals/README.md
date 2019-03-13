54c4b16646e8e6592d39edbb37a7c469786d25dc
dev-pasa <dev5@uw.edu>
2019-03-13 09:04:22 -0700

    Unique words

---
+++ Challenges/2dArrayUniqueVals/README.md
@@ -0,0 +1,25 @@
+# Unique integers in a 2D array
+
+
+## Challenge
+- Create a method that output unique values in a 2D array
+
+- Ensure your tests are passing
+
+## Approach & Efficiency
+1. A method to remove collision using Hashset
+2. create a hash set to remove collison among duplicates
+3. create a list to return the data
+4. Add all the values to hashset
+5. Add allt the values to a list
+6. Return the list
+
+## Solution
+![Remove Duplicates](/Assets/RemoveDuplicates.jpg)
+
+### Efficiency
+* Space Complexity
+O(n^2) for a 2d array 
+
+* Time Complexity
+O(n2) for two for loops in the main method
---
+++ Challenges/2dArrayUniqueVals/UniqueVals/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueVals
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] input = new int[,] { { 1, 2, 3, 4, 5 }, { 4, 5, 6, 7, 8 }, { 6, 7, 8, 9, 10 } };
+            //UniqueVals(input);
+
+            Console.WriteLine("Print all the values in the given input");
+            for(int i = 0; i < input.GetLength(0); i++)
+            {
+                for(int j = 0; j < input.GetLength(1); j++)
+                {
+                    Console.Write(input.GetValue(i,j) + " ");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Output after passing the method to remove duplicates");
+            UniqueVals(input);
+
+        }
+        /// <summary>
+        /// A method to remove collision using Hashset
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static List<int> UniqueVals(int[,] input)
+        {
+            List<int> output = new List<int>();
+            //create a hash set to remove collison among duplicates
+            HashSet<int> set = new HashSet<int>();
+            // add all the values in the hashset
+            foreach (int ints in input)
+            {
+                set.Add(ints);
+            }
+            //Add all the values of hashset to list
+            foreach (int val in set)
+                output.Add(val);
+
+            return output;
+        }
+
+
+       
+    }
+}
