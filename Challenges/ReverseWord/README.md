4c5d0be95ca10f2181d5506c43b71b49b4e59153
dev-pasa <dev5@uw.edu>
2019-03-12 19:05:26 -0700

    all set

---
+++ Challenges/ReverseWord/README.md
@@ -0,0 +1,23 @@
+# Reverse the Chars in a Word or String
+- 
+
+## Challenge
+- Create a method that reverses the Chars in a word or String.
+
+## Approach & Efficiency
+1. Create a method that reverses the chars in a word
+2. Create a char array that has all the characters from the input using toCharArray()
+3. Do a for loop from the front and the back at each index of the array
+4. Reverse the chars in the character array with a for loop
+5. Return a new string that has the reversed values in the character array
+
+
+## Solution
+![Reverse Word](/Assets/ReverseWord.jpg)
+
+### Efficiency
+* Space Complexity
+O(n) as each chars in the array has to be traversed
+
+* Time Complexity
+O(n) as a new array is created
---
+++ Challenges/ReverseWord/WordReverse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WordReverse
+{
+    public class Program
+    {
+        /// <summary>
+        /// Run the test on the main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The original word is: Hellow World");
+            Console.WriteLine("After reversing the input: ");
+            Console.WriteLine(ReverseCharsInWord("Hello World"));
+        }
+
+        /// <summary>
+        /// Create a method that reverses the chars in a word
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static String ReverseCharsInWord(String input)
+        {
+            // create a char array that has all the characters from the input
+            char[] charsInWord = input.ToCharArray();
+
+            // do a for loop from the front and the back at each index of the array
+            for (int i = 0, j = charsInWord.Length - 1; i < j; i++, j--)
+            {
+                //reverse the chars in the character array
+                char temp = charsInWord[i];
+                charsInWord[i] = charsInWord[j];
+                charsInWord[j] = temp;
+            }
+            //return a new string that has the reversed values in the character array
+            return new String(charsInWord);
+        }
+    }
+}
